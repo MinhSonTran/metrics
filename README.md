a715777abae2b4c4c75ada4fac5a2a8761df7ba5
Brandon Keepers <bkeepers@github.com>
2017-07-17 11:34:37 +0200

    Initial metrics package

---
+++ README.md
@@ -0,0 +1,7 @@
+# Probot Metrics
+
+Track metrics for [probot](https://github.com/probot/probot) deployments
+
+Metrcis collected:
+
+- The name, action, and GitHub App installation id for each delivered event
---
+++ index.js
@@ -0,0 +1,24 @@
+const ua = require('universal-analytics');
+
+const GOOGLE_ANALYTICS_ID = process.env.GOOGLE_ANALYTICS_ID;
+const agentOptions = {strictCidFormat: false, https: true};
+
+module.exports = robot => {
+  robot.log.trace({id: GOOGLE_ANALYTICS_ID}, 'Tracking metrics with Google Analytics');
+
+  robot.on('*', async context => {
+    const uid = context.payload.installation.id;
+    const visitor = ua(GOOGLE_ANALYTICS_ID, uid, agentOptions);
+
+    const params = {
+      ec: context.event,
+      ea: context.payload.action
+    };
+
+    robot.log.trace(params, 'Reporting metric');
+
+    return new Promise((resolve, reject) => {
+      visitor.event(params, err => err ? reject(err) : resolve());
+    });
+  });
+};
