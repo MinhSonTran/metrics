b37ac54ca8cef3aff7323c9c7b0512707e6605a6
Brandon Keepers <bkeepers@github.com>
2017-07-20 09:19:46 -0500

    Add test for metric reporting

--- index.js
+++ index.js
@@ -4,6 +4,11 @@ const GOOGLE_ANALYTICS_ID = process.env.GOOGLE_ANALYTICS_ID;
 const agentOptions = {strictCidFormat: false, https: true};
 
 module.exports = robot => {
+  if (!GOOGLE_ANALYTICS_ID) {
+    robot.log.trace('Set `GOOGLE_ANALYTICS_ID` to track metrics with Google Analytics');
+    return;
+  }
+
   robot.log.trace({id: GOOGLE_ANALYTICS_ID}, 'Tracking metrics with Google Analytics');
 
   robot.on('*', async context => {
